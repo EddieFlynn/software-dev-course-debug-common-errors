2d0dbd486044b346cd2cfb775e370d5f064d7b0a
Eddie Flynn <eddieflynn4444@gmail.com>
2025-07-19 16:02:11 -0500

    Prime

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -24,39 +24,41 @@ Think about which debugging methods you found most useful and how you might appl
 // Description:
 // This program is intended to display a simple prompt in the console but fails to run.
 
-console.log("Welcome to the bootcamp
+console.log("Welcome to the bootcamp");
 
 // What’s Wrong?
-
+// syntax error - added closing ", ), and ;
 
 // Program B
 // Description:
 // This code attempts to multiply each number in an array by 2 and display the results. However, it crashes at runtime.
 
-let numbers = [2, 4, "eight"];
+let numbers = [2, 4, 8];
 for (let i = 0; i < numbers.length; i++) {
   let doubled = numbers[i] * 2;
   console.log(doubled);
 }
 
 // What’s Wrong?
-
+// "eight" is not a number recognized by the program - replaced with number 8
 
 
 // Program C (Logic Error)
 // Description:
 // This snippet of code is supposed to check if a given number is prime (i.e., divisible only by 1 and itself). However, it incorrectly marks some numbers as prime or not prime.
 
-function isPrime(num) {
-  if (num < 2) return false;
+function isPrime(num) { 
+  if (num < 2) return true;
   for (let i = 2; i < num; i++) {
     if (num % i === 0) {
-      return true;  // Supposed to indicate num is NOT prime
+      return false;  // Supposed to indicate num is NOT prime
     }
   }
-  return false; // Supposed to indicate num IS prime
+  return true; // Supposed to indicate num IS prime
 }
 
 console.log(isPrime(7)); // Expected true but gets false
 
 // What’s Wrong?
+// logic issue - returned the opposite of a prime number
+// swapped return statements 
